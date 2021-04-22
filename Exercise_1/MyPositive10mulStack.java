b2c940e8c99bf9315032628265fb6814616d7717
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-04-22 19:07:43 +0300

    Add files via upload

---
+++ Exercise_1/MyPositive10mulStack.java
@@ -0,0 +1,30 @@
+package com.company;
+
+
+
+public class MyPositive10mulStack implements IMyPositive10mulStack{
+    @Override
+    public void push(int item) {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public int pop() {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public int peek() {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public void clear() {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public int count() {
+        throw new RuntimeException();
+    }
+}
---
+++ Exercise_1/MyPositive10mulStackTest.java
@@ -0,0 +1,66 @@
+package com.company;
+
+import org.junit.Test;
+import static junit.framework.TestCase.assertEquals;
+
+public class MyPositive10mulStackTest {
+
+    @Test
+    public void test_MyPositive10mulStack_push_pos1()
+    {
+        MyPositive10mulStack stack = new MyPositive10mulStack();
+        stack.push(10);
+
+        // non atomic
+        assertEquals(stack.count(), 1);
+        assertEquals(stack.peek(), 10);
+
+    }
+
+    @Test
+    public void test_MyPositive10mulStack_pop_pos1()
+    {
+        MyPositive10mulStack stack = new MyPositive10mulStack();
+        stack.push(10);
+        stack.pop();
+
+        // non atomic
+        assertEquals(stack.count(), 0);
+    }
+
+    @Test
+    public void test_MyPositive10mulStack_peek_pos1()
+    {
+        MyPositive10mulStack stack = new MyPositive10mulStack();
+        stack.push(10);
+        int el = stack.peek();
+
+        // non atomic
+        assertEquals(stack.count(), 1);
+        assertEquals(el, 10);
+    }
+
+    @Test
+    public void test_MyPositive10mulStack_clear_pos1()
+    {
+        MyPositive10mulStack stack = new MyPositive10mulStack();
+        stack.push(10);
+        stack.clear();
+
+        // non atomic
+        assertEquals(stack.count(), 0);
+    }
+
+    @Test
+    public void test_MyPositive10mulStack_count_pos1()
+    {
+        MyPositive10mulStack stack = new MyPositive10mulStack();
+        stack.push(10);
+        int size = stack.count();
+
+        // non atomic
+        assertEquals(size, 1);
+        assertEquals(stack.peek(), 10);
+    }
+
+}
